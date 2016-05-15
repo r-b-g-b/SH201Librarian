a9cf43a9c50ee7bfa62a337c5a08feb9fe2b41c4
Robert Gibboni <galileo@gmail.com>
2016-05-15 14:24:05 -0700

    reworked backup code, can now easily move patches to and from the sh 201. minor other changes, can refer to patches by letter/number combo, e.g. A4, B1

--- librarian.py
+++ librarian.py
@@ -2,9 +2,81 @@ from mido import Message, open_ioport, get_ioport_names, write_syx_file, read_sy
 import time
 import os
 import argparse
+from itertools import product
+
+program_name_to_number = lambda pname: 'abcd'.index(pname[0].lower())*8 + int(pname[1])-1
+
+class SH201SysExMessage(object):
+    _HEADER = (65, 127, 0, 0, 22)
+    _MSGTYPECODES = {'get': (17,),
+                     'put': (18,)
+                    }
+    def __init__(self, msg_type):
+        self.message = Message('sysex')
+        self.msg_type = msg_type
+        
+    def _compile_message(self):
+        if hasattr(self, 'addr') and hasattr(self, 'data'):
+            msg = self._HEADER + self._TXRX
+            msg += self.addr + self.data
+            msg += (self._compute_checksum(),)
+            self.message.data = msg
+            
+    def _compute_checksum(self):
+        return 128 - sum(self.addr + self.data)%128
+
+    @property
+    def msg_type(self):
+        return self.__msg_type
+    @msg_type.setter
+    def msg_type(self, msg_type):
+        self.__msg_type = msg_type
+        self._TXRX = self._MSGTYPECODES[msg_type]
+        
+    @property
+    def payload(self):
+        return self.message.data
+    @payload.setter
+    def payload(self, payl):
+        '''
+        Set the complete message payload
+        Input
+        ------
+        payl: a complete message payload (not including start and stop flags)
+        '''
+        self.msg_type = [k for k,v in self._MSGTYPECODES.items() if v==(payl[5],)][0]
+        self.addr = payl[6:10]
+        self.data = payl[10:-2]
+        self.message.data = payl
+        
+    @property
+    def data(self):
+        return self.__data
+    @data.setter
+    def data(self, dat):
+        self.__data = dat
+        self._compile_message()
+
+    @property
+    def addr(self):
+        return self.__addr
+    @addr.setter
+    def addr(self, addr):
+        self.__addr = addr
+        self.__program = addr[1]
+        self._compile_message()
+        
+    @property
+    def program(self):
+        return self.__program
+    @program.setter
+    def program(self, p):
+        addr = list(self.addr) if hasattr(self, 'addr') else [32,None,0,0]
+        addr[1] = p
+        self.addr = tuple(addr)
 
 class SH201Librarian(object):
-    _DUMP = Message('sysex', data=[65, 127, 0, 0, 22, 17, 16, 0, 0, 0, 0, 0, 9, 88, 15])
+    _REQPATCH = (0, 0, 21, 66)
     def __init__(self, ioport_name=None):
         if ioport_name is None:
             ioport_name = self._gui_select_ioport()
@@ -18,38 +90,65 @@ class SH201Librarian(object):
             return ioport_names[ioport_num]
         except OSError as errmsg:
             print(errmsg)
-            
+    
+    def _generate_download_patch_message(self, program_number):
+        req = SH201SysExMessage(msg_type='get')
+        req.data = self._REQPATCH
+        req.program = program_number
+        return req.message
+    
     def change_program(self, p):
         req = Message('program_change', program=p)
         self.port.send(req)
     
-    def download_patch(self, program_number, timeout=3):
-        self.change_program(program_number)
+    def download_patch(self, program_number, n_messages=21, timeout=3):
+        '''
+        A patch is completely specified by 21 messages
+        '''
+        if type(program_number) is str:
+            program_number = program_name_to_number(program_number)
         patch = []
         tic = time.time()
         i = 0
-        self.port.send(self._DUMP)
-        while len(patch)<=10:
+        req = self._generate_download_patch_message(program_number)
+        print(req.hex())
+        self.port.send(req)
+        while len(patch)<=n_messages:
             i += 1
-            rcv = self.port.receive(block=False)
+            rcv = self.port.receive()
             toc = time.time()
             if (toc-tic)>timeout: break
             if rcv is None:
                 time.sleep(0.1)
                 continue
-            if rcv.type=='sysex':
-                
+            # filter sysex messages originating from the desired patch number
+            if (rcv.type=='sysex') and (rcv.data[7]==program_number):
                 patch.append(rcv)
         return patch
-    def upload_patch(self, destination, patch):
+    
+    def upload_patch(self, program_number, patch):
+        '''
+        Overwrites the patch settings
+        Input
+        --------
+        program_number (int) number of the patch you want to overwrite. A1 is 0, D8 is 32
+        patch (list of sysex messages)
+        '''
         if type(patch) is str:
             patch = read_syx_file(patch)
-        self.change_program(destination)
-        [self.port.send(m) for m in patch]
-        
+        if type(program_number) is str:
+            program_number = program_name_to_number(program_number)
+        for m in patch:
+            mess = SH201SysExMessage('put')
+            mess.payload = m.data
+            mess.program = program_number
+            self.port.send(mess.message)
+    
     def backup_all(self, directory, prefix='patch_'):
-        for p in range(4*8):            
-            write_syx_file(os.path.join(directory, prefix+'%2.2i.syx'%p), self.download_patch(p))
+        for i, (bank, prog) in enumerate(product(['A','B','C','D'], range(1,9))):
+            program_name = '%s%i' % (bank, prog)
+            print(program_name, end=' ', flush=True)
+            write_syx_file(os.path.join(directory, prefix+'%s.syx'%p), self.download_patch(i))
             
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description = '''
